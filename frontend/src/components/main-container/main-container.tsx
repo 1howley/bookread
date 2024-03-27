deacf072055a0c85f06c24a21066ca1acf3c9190
Ana Paula Ferreira <bielgabrielfs61@gmail.com>
2024-03-26 21:05:18 -0300

    feat: main-container

---
+++ frontend/src/components/main-container/main-container.tsx
@@ -0,0 +1,16 @@
+import './main-container.css'
+
+function MainContainer() {
+
+    return (
+        <div className="containerView">
+            <img src="https://m.media-amazon.com/images/I/81SQWCqtWlL._SY466_.jpg" alt="Capa do Livro" />
+            {/*  stars */}
+            <div className="bar"></div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia et, temporibus veniam sapiente necessitatibus maxime odio cupiditate velit doloribus placeat obcaecati veritatis, blanditiis voluptatibus laboriosam, repellendus quae asperiores consequuntur!</h4>
+        </div>
+    )
+}
+    
+export default MainContainer
+    
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,12 +1,15 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import MainContainer from '../../components/main-container/main-container';
+import './home.css'
 
 function Home() {
 
     return (
-        <>
-        <ChevronLeft/>
-        <ChevronRight/>
-        </>
+        <div className='view'>
+            <ChevronLeft color='black' size={50} className='arrow' />
+            <MainContainer/>
+            <ChevronRight color='black' size={50} className='arrow' />
+        </div>
     )
 }
     
