e4eec2ee9086bf9b73a8e020c66cef67fa03df91
Ana Paula Ferreira <bielgabrielfs61@gmail.com>
2024-03-26 21:50:15 -0300

    imp: reponsive content

--- frontend/src/components/main-container/main-container.tsx
+++ frontend/src/components/main-container/main-container.tsx
@@ -7,7 +7,10 @@ function MainContainer() {
             <img src="https://m.media-amazon.com/images/I/81SQWCqtWlL._SY466_.jpg" alt="Capa do Livro" />
             {/*  stars */}
             <div className="bar"></div>
-            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia et, temporibus veniam sapiente necessitatibus maxime odio cupiditate velit doloribus placeat obcaecati veritatis, blanditiis voluptatibus laboriosam, repellendus quae asperiores consequuntur!</h4>
+            <div className="text">
+                <h2>O fazedor de velhos</h2>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia et, temporibus veniam sapiente necessitatibus maxime odio cupiditate velit doloribus placeat obcaecati veritatis, blanditiis voluptatibus laboriosam, repellendus quae asperiores consequuntur!</h4>
+            </div>
         </div>
     )
 }
